8c2981a16f8b69acd4a6207fe155c972964c5fc8
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2015-02-15 14:07:27 -0400

    adaptation for dynamic management

--- js/pushy.js
+++ js/pushy.js
@@ -1,107 +1,149 @@
 /*! Pushy - v0.9.2 - 2014-9-13
-* Pushy is a responsive off-canvas navigation menu using CSS transforms & transitions.
+* Pushy is a responsive off-canvas navigation menu
+* using CSS transforms & transitions.
 * https://github.com/christophery/pushy/
 * by Christopher Yee */
 
-$(function() {
-	var pushy = $('.pushy'), //menu css class
-		body = $('body'),
-		container = $('#container'), //container css class
-		push = $('.push'), //css class to add pushy capability
-		siteOverlay = $('.site-overlay'), //site overlay
-		pushyClass = "pushy-left pushy-open", //menu position & menu open class
-		pushyActiveClass = "pushy-active", //css class to toggle site overlay
-		containerClass = "container-push", //container open class
-		pushClass = "push-push", //css class to add pushy capability
-		menuBtn = $('.menu-btn, .pushy a'), //css classes to toggle the menu
-		menuSpeed = 200, //jQuery fallback menu speed
-		menuWidth = pushy.width() + "px"; //jQuery fallback menu width
+!function(){
+    var settings={
+        pushy:'.pushy' //menu css class
+      , body:'body'
+      , container:'#content' //container css class
+      , push:'.push' //css class to add pushy capability
+      , siteOverlay:'.site-overlay' //site overlay
+      , pushyClass:'pushy-left pushy-open' //menu position & menu open class
+      , pushyActiveClass:'pushy-active' //css class to toggle site overlay
+      , containerClass:'container-push' //container open class
+      , pushClass:'push-push' //css class to add pushy capability
+      , menuBtn:'.menu-btn, .pushy a' //css classes to toggle the menu
+      , menuSpeed:200 //jQuery fallback menu speed
+    }
+    
+    var pushy=function(c){
+        var config;
 
-	function togglePushy(){
-		body.toggleClass(pushyActiveClass); //toggle site overlay
-		pushy.toggleClass(pushyClass);
-		container.toggleClass(containerClass);
-		push.toggleClass(pushClass); //css class to add pushy capability
-	}
+        if(c){
+            config=$.extend(settings,c);
+        }else{
+            config=settings;
+        }
 
-	function openPushyFallback(){
-		body.addClass(pushyActiveClass);
-		pushy.animate({left: "0px"}, menuSpeed);
-		container.animate({left: menuWidth}, menuSpeed);
-		push.animate({left: menuWidth}, menuSpeed); //css class to add pushy capability
-	}
+        var pushy=$(config.pushy)
+          , body=$(config.body)
+          , container=$(config.container)
+          , push=$(config.push)
+          , siteOverlay=$(config.siteOverlay)
+          , pushyClass=config.pushyClass
+          , pushyActiveClass=config.pushyActiveClass
+          , containerClass=config.containerClass
+          , pushClass=config.pushClass
+          , menuBtn=$(config.menuBtn)
+          , menuSpeed=config.menuSpeed
+          , menuWidth=pushy.width()+'px' //jQuery fallback menu width
 
-	function closePushyFallback(){
-		body.removeClass(pushyActiveClass);
-		pushy.animate({left: "-" + menuWidth}, menuSpeed);
-		container.animate({left: "0px"}, menuSpeed);
-		push.animate({left: "0px"}, menuSpeed); //css class to add pushy capability
-	}
+        this.togglePushy=function(){
+            body.toggleClass(pushyActiveClass); //toggle site overlay
+            pushy.toggleClass(pushyClass);
+            container.toggleClass(containerClass);
+            push.toggleClass(pushClass); //css class to add pushy capability
+        }
 
-	//checks if 3d transforms are supported removing the modernizr dependency
-	cssTransforms3d = (function csstransforms3d(){
-		var el = document.createElement('p'),
-		supported = false,
-		transforms = {
-		    'webkitTransform':'-webkit-transform',
-		    'OTransform':'-o-transform',
-		    'msTransform':'-ms-transform',
-		    'MozTransform':'-moz-transform',
-		    'transform':'transform'
-		};
+        this.openPushyFallback=function(){
+            body.addClass(pushyActiveClass);
+            pushy.animate({left:'0px'},menuSpeed);
+            container.animate({left:menuWidth},menuSpeed);
+            push.animate({
+                left:menuWidth},menuSpeed); //css class to add pushy capability
+        }
 
-		// Add it to the body to get the computed style
-		document.body.insertBefore(el, null);
+        this.closePushyFallback=function(){
+            body.removeClass(pushyActiveClass);
+            pushy.animate({left:'-'+menuWidth},menuSpeed);
+            container.animate({left:'0px'},menuSpeed);
+            push.animate({
+                left:'0px'},menuSpeed); //css class to add pushy capability
+        }
 
-		for(var t in transforms){
-		    if( el.style[t] !== undefined ){
-		        el.style[t] = 'translate3d(1px,1px,1px)';
-		        supported = window.getComputedStyle(el).getPropertyValue(transforms[t]);
-		    }
-		}
+        //checks if 3d transforms are supported
+        //removing the modernizr dependency
+        cssTransforms3d=(function csstransforms3d(){
+            var el=document.createElement('p')
+              , supported=false
+              , transforms={
+                    'webkitTransform':'-webkit-transform'
+                  , 'OTransform':'-o-transform'
+                  , 'msTransform':'-ms-transform'
+                  , 'MozTransform':'-moz-transform'
+                  , 'transform':'transform'
+                }
 
-		document.body.removeChild(el);
+            // Add it to the body to get the computed style
+            document.body.insertBefore(el,null);
 
-		return (supported !== undefined && supported.length > 0 && supported !== "none");
-	})();
+            for(var t in transforms){
+                if(el.style[t]!==undefined){
+                    el.style[t]='translate3d(1px,1px,1px)';
+                    supported=window.getComputedStyle(el)
+                                    .getPropertyValue(transforms[t]);
+                }
+            }
 
-	if(cssTransforms3d){
-		//toggle menu
-		menuBtn.click(function() {
-			togglePushy();
-		});
-		//close menu when clicking site overlay
-		siteOverlay.click(function(){ 
-			togglePushy();
-		});
-	}else{
-		//jQuery fallback
-		pushy.css({left: "-" + menuWidth}); //hide menu by default
-		container.css({"overflow-x": "hidden"}); //fixes IE scrollbar issue
+            document.body.removeChild(el);
 
-		//keep track of menu state (open/close)
-		var state = true;
+            return (supported!==undefined&&
+                    supported.length>0&&
+                    supported!=='none');
+        })();
 
-		//toggle menu
-		menuBtn.click(function() {
-			if (state) {
-				openPushyFallback();
-				state = false;
-			} else {
-				closePushyFallback();
-				state = true;
-			}
-		});
+        //keep track of menu state (open/close)
+        var state=true;
 
-		//close menu when clicking site overlay
-		siteOverlay.click(function(){ 
-			if (state) {
-				openPushyFallback();
-				state = false;
-			} else {
-				closePushyFallback();
-				state = true;
-			}
-		});
-	}
-});
+        this.openEvent=function(){
+            if(cssTransforms3d){
+                //toggle menu
+                menuBtn.click(function() {
+                    togglePushy();
+                });
+            }else{
+                //jQuery fallback
+                pushy.css({left:'-'+menuWidth}); //hide menu by default
+                container.css({
+                    'overflow-x':'hidden'}); //fixes IE scrollbar issue
+
+                //toggle menu
+                menuBtn.click(function(){
+                    if(state){
+                        openPushyFallback();
+                        state=false;
+                    }else{
+                        closePushyFallback();
+                        state=true;
+                    }
+                });
+            }
+        }
+
+        this.closeEvent=function(){
+            if(cssTransform3d){
+                //close menu when clicking site overlay
+                siteOverlay.click(function(){ 
+                    togglePushy();
+                });
+            }else{
+                //close menu when clicking site overlay
+                siteOverlay.click(function(){ 
+                    if(state){
+                        openPushyFallback();
+                        state=false;
+                    }else{
+                        closePushyFallback();
+                        state=true;
+                    }
+                });
+            }
+        }
+    }
+
+    this.pushy=pushy;
+}();
+
